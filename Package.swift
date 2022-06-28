798142bf5368832eca3007ec85017a3a5759a517
Vasilij Orlov <vasilij_orlov@stylesoft.by>
2022-06-28 18:38:49 +0200

    update the manifest

--- Package.swift
+++ Package.swift
@@ -4,30 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
-    platforms: [.iOS(.v13)],
+    name: "ARFurnitureSDK",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            name: "ARFurnitureSDK",
+            targets: ["ARFurnitureSDK"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyLibrary"
-        ),
-       // .binaryTarget(
-       //     name: "CSConnectPackage",
-       //     path: "/Users/vasiliorlov/Documents/xcframework/CSConnectFramework.xcframework"
-       // ),
-        .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"]),
+        .binaryTarget(name: "ARFurnitureSDK",
+                      path: "CSConnectFramework.xcframework"),
     ]
 )
